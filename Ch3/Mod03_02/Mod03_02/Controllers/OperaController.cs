cce5434020e5dbe9ae9467738928bcb178ca6aa8
PenziSu <penzi.su@gmail.com>
2017-05-11 20:09:20 +0800

    20170511-break time

---
+++ Ch3/Mod03_02/Mod03_02/Controllers/OperaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod03_02.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        public ActionResult Index()
+        {
+            ViewBag.OperaID = "1";
+            ViewBag.Title = "FFFFF";
+            ViewBag.Year = 1983;
+            ViewBag.Composer = "Mark";
+            return View();
+        }
+    }
+}

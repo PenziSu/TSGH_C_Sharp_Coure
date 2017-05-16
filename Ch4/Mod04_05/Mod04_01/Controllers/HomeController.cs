d99f95f6d02d74da20c8e84dee6a78a61c2fabb2
PenziSu <penzi.su@gmail.com>
2017-05-16 20:58:57 +0800

    練習4-5

---
+++ Ch4/Mod04_05/Mod04_01/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod04_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Lab04_05
+        public ActionResult GetVideo()
+        {
+            return File("~/Content/movie.mp4", "video/mp4");
+        }
+    }
+}

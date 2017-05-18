ac6bb559bcfced42d927c5f8a086221ed9299751
PenziSu <penzi.su@gmail.com>
2017-05-18 19:23:41 +0800

    20170518-1

---
+++ Ch4/Mod04_07/Mod04_07/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mod04_07.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="subject不可為空白")]
+        public string Subject { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string HtmlComment { get; set; }
+
+        public int Price { get; set; }
+
+        public bool IsApprove { get; set; }
+    }
+}

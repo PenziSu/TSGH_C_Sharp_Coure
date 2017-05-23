db9140258b2ec10f43c4166753f2d7d109632020
PenziSu <penzi.su@gmail.com>
2017-05-23 19:12:33 +0800

    ch6

---
+++ Ch6/Mod06_04/Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,173 @@
+﻿using Mod02_01.DAL;
+using Mod02_01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        // GET: Opera/Index?operaId=111&title=xx&year=1923&composer=John
+        //public ActionResult Index(Opera opera)
+        //{
+        //    //var test = ModelState.IsValid;
+        //    //Opera o = new Opera()
+        //    //{
+        //    //    OperaID = opera.OperaID,
+        //    //    Title = opera.Title,
+        //    //    Year = opera.Year,
+        //    //    Composer = opera.Composer
+        //    //};
+
+        //    return View(opera);
+        //}
+
+        // Lab2_4
+        // GET: Opera/Index        
+        public ActionResult Index()
+        {
+            OperaContext context = new OperaContext();
+            Debug.WriteLine("Opera.Index");
+            return View(context.Operas.ToList());
+        }
+
+        //Lab3_3
+        //GET : Opera/Details/1
+        //GET : Opera/Details?id=1
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OperaContext context = new OperaContext();
+            Opera o = context.Operas.Find(id);
+            if(o == null)
+            {
+                return HttpNotFound();
+            }
+            return View(o);
+        }
+
+        //Lab3_4
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create( Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                OperaContext context = new OperaContext();
+                context.Operas.Add(opera);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(opera);
+        }
+
+        //Lab3_7
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OperaContext context = new OperaContext();
+            Opera o = context.Operas.Find(id);
+            if(o == null)
+            {
+                return HttpNotFound();
+            }
+            return View(o);
+        }
+
+        [HttpPost]
+        public ActionResult Edit( Opera opera)
+        {
+            if (ModelState.IsValid)
+            {
+                OperaContext context = new OperaContext();
+                context.Entry(opera).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(opera);
+        }
+
+        //Lab3_8
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    OperaContext context = new OperaContext();
+        //    Opera o = context.Operas.Find(id);
+        //    if (o == null)
+        //        return HttpNotFound();
+        //    return View(o);
+        //}
+
+        ////POST:
+        ////POST:
+        //[HttpPost, ActionName("Delete")]
+        //public ActionResult DeleteConfirmed(int? id)
+        //{
+        //    OperaContext context = new OperaContext();
+        //    Opera o = context.Operas.Find(id);
+        //    context.Operas.Remove(o);
+        //    context.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            OperaContext context = new OperaContext();
+            Opera o = context.Operas.Find(id);
+            if (o == null)
+                return HttpNotFound();
+            context.Operas.Remove(o);
+            context.SaveChanges();
+            return RedirectToAction("Index");            
+        }
+
+        //Lab04_06
+        public ActionResult FilterData(int number)
+        {
+            OperaContext context = new OperaContext();
+            //LINQ
+            var query = (from o in context.Operas          //從context.Operas查詢所有結果
+                         orderby o.Year descending         //將查詢結果以年分降冪排序
+                         select o).Take(number).ToList();  //接著取出number指定的數量資料
+            return View("Index", query);  //將結果交給View，這個View是Index method的View
+        }
+
+        //Lab06_01
+        [Route("opera/title/{title=Wozzeck}")]
+        public ActionResult DetailsByTitle(string title)
+        {
+            ViewBag.mycontroller = RouteData.Values["controller"];
+            ViewBag.myaction = RouteData.Values["action"];
+            ViewBag.mytitle = RouteData.Values["title"];
+
+            OperaContext context = new OperaContext();
+            Opera opera = context.Operas.FirstOrDefault<Opera>(o => o.Title == title);
+            if (opera == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("Details", opera);
+        }
+    }
+}
---
+++ Ch6/Mod06_04/Mod02_01/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Mod02_01.DAL;
+using System.Data.Entity;
+
+namespace Mod02_01
+{    
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //Lab2_4: 啟用種子資料建立
+            Database.SetInitializer<OperaContext>(new OperasInitializer());
+
+            //Lab3_9
+            GlobalFilters.Filters.Add(new LogActionFilter());
+        }
+    }
+}

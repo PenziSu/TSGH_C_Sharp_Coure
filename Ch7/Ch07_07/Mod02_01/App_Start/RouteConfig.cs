639e9c498788c06ed31f5a1d5262ac4ce13aea22
PenziSu <penzi.su@gmail.com>
2017-05-23 21:22:58 +0800

    2017-05-23

---
+++ Ch7/Ch07_07/Mod02_01/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mod02_01
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {            
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Lab06_02
+            routes.MapMvcAttributeRoutes();
+
+            //Lab06_01
+            //routes.MapRoute(
+            //    name: "OperaTitleRoute",
+            //    url: "opera/title/{title}",
+            //    defaults: new { controller = "opera", action = "DetailsByTitle", title = UrlParameter.Optional }
+            //);            
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

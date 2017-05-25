c852b6647bcd97d389745e4afb218596f9a0ce7e
PenziSu <penzi.su@gmail.com>
2017-05-25 18:39:56 +0800

    CH8-1

---
+++ Ch8/Mod08_01/Mod08_01/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod08_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Home/HelloWorld
+        public ActionResult HelloWorld()
+        {
+            ViewBag.message = "HelloWorld: " + DateTime.Now.ToString();
+            return PartialView();
+
+        }
+    }
+}

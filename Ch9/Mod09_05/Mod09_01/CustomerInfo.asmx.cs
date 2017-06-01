3538e93073f58ca91d9a81be4be264d481b81812
PenziSu <penzi.su@gmail.com>
2017-06-01 19:38:09 +0800

    Ch09_06

--- Ch9/Mod09_05/Mod09_01/CustomerInfo.asmx.cs
+++ Ch9/Mod09_05/Mod09_01/CustomerInfo.asmx.cs
@@ -9,9 +9,9 @@ namespace Mod09_01
     /// <summary>
     /// Summary description for CustomerInfo
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    [System.ComponentModel.ToolboxItem(false)]
+    //[WebService(Namespace = "http://tempuri.org/")]
+    //[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    //[System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     [System.Web.Script.Services.ScriptService] //<<=重點, 註解解開後就可以回傳其他格式, 如Json
     public class CustomerInfo : System.Web.Services.WebService

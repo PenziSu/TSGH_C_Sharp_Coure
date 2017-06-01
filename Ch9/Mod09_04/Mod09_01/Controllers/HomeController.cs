2eecc2c6e3dc82121a76fffbb06b5d2828af30e6
PenziSu <penzi.su@gmail.com>
2017-06-01 18:10:56 +0800

    Ch09_04

---
+++ Ch9/Mod09_04/Mod09_01/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod09_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SelectorDemo
+        public ActionResult SelectorDemo()
+            
+        {
+            return View();
+        }
+
+        // GET: index2
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}

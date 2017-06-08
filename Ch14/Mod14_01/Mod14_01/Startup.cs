4acdd6429a24bfb82580765d7868c5bec65cd886
PenziSu <penzi.su@gmail.com>
2017-06-08 20:53:13 +0800

    FINISH

---
+++ Ch14/Mod14_01/Mod14_01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mod14_01.Startup))]
+namespace Mod14_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
